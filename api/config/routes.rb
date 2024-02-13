0661b29a78a3f956a8982342e9ab61f14e168ba5
Makoto Kitamura <admin@mktktmr.net>
2024-02-13 23:33:50 +0900

    add devise

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  mount_devise_token_auth_for 'User', at: 'auth'
+
+  resources :users
 end
